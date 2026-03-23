ccee97d76d82643f7ae5bfffa0de9b627b2c22bc
maramire2001 <marioantonioramirezbarajas@gmail.com>
2026-03-23 14:51:39 -0600

    Map root URL and apply premium frontend design

--- evaluation_app/urls.py
+++ evaluation_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.submit_assignment, name='home'),
     path('submit/', views.submit_assignment, name='submit'),
     path('export/', views.export_grades, name='export'),
 ]
