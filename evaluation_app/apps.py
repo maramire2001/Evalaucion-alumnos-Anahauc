9482194839ace9600796f10d16cfdf6543ea21b5
maramire2001 <marioantonioramirezbarajas@gmail.com>
2026-03-23 15:23:51 -0600

    Add auto-superuser creation on migrate

--- evaluation_app/apps.py
+++ evaluation_app/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_superuser(sender, **kwargs):
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@anahuac.mx', 'Anahuac2026!')
 
 class EvaluationAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'evaluation_app'
+
+    def ready(self):
+        post_migrate.connect(create_default_superuser, sender=self)
